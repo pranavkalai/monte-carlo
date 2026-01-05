6d6064033148c0970b939c950b69f6fdc8145641
Pranav Kalaiselvan <pranavkalai04@gmail.com>
2026-01-05 11:35:27 -0600

    Added a main program that can run both cpu and gpu versions at the same time

---
+++ main.cpp
@@ -0,0 +1,45 @@
+// %%writefile main.cpp
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char* argv[]) {
+
+    char* args_cpu[] = {(char*)"./mc_cpu", (char*)"1000", nullptr};
+    char* args_gpu[] = {(char*)"./mc_gpu", (char*)"1000", nullptr};
+
+    if (argc == 2) {
+        args_cpu[1] = argv[1];
+        args_gpu[1] = argv[1];
+    }
+    else if (argc > 2) {
+        std::cerr << "Too many arguements provided" << std::endl;
+        return -1;
+    }
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        std::cerr << "Fork failed" << std::endl;
+        return 1;
+    } 
+    else if (pid == 0) {
+        // Child process
+        if (execvp(args_cpu[0], args_cpu) < 0) {
+            std::cerr << "Error executing CPU program" << std::endl;
+            return 1;
+        }
+    } 
+    else {
+        // Parent process
+        waitpid(pid, nullptr, 0); // Wait for child process to finish
+        std::cout << std::endl;
+        if (execvp(args_gpu[0], args_gpu) < 0) {
+            std::cerr << "Error executing GPU program" << std::endl;
+            return 1;
+        }
+    }
+
+    return 0;
+    
+}
